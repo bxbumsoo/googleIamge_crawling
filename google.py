9abca0031551722030e15434733b2708eb3cb3ac
bumsoo kim <bxbumsoo@naver.com>
2020-12-28 15:19:45 +0900

    first commit

---
+++ google.py
@@ -0,0 +1,8 @@
+from google_images_download import google_images_download  # importing the library
+
+response = google_images_download.googleimagesdownload()  # class instantiation
+
+arguments = {"keywords": "박정희",
+             "limit": 50, "print_urls": True, "format": "jpg"}  # creating list of arguments
+paths = response.download(arguments)  # passing the arguments to the function
+print(paths)  # printing absolute paths of the downloaded images
